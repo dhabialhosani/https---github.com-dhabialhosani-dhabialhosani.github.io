e0cb482e6d3afd56e92dbbf37e969c3897b7ea67
dhabialhosani <daa9591@nyu.edu>
2022-12-12 14:28:08 +0400

    go

---
+++ Final Project/app.js
@@ -0,0 +1,24 @@
+//This javascript function allows for the transition hover on the left
+//Hides in active pages to keep the selected page in display
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+
+
+
+
+
+
+
+
+
